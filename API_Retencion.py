09fd7ef755ccf9fed9f4d621a878db9a9f232f4a
EstebanR16 <est1696@hotmail.com>
2024-03-26 15:09:30 -0600

    Modificacion de la logica de las consultas, Todos los archivos .py y .ipynb

--- API_Retencion.py
+++ API_Retencion.py
@@ -17,12 +17,11 @@ data['Mes de Abandono (Churn)'] = data['Mes de Abandono (Churn)'].dt.strftime('%
 data['Mes Registro'] = data['Mes Registro'].dt.strftime('%Y-%m')
 
 
-
 def Clientes_por_Mes(mes: str):
     try:
         # Verifica si la columna mes existe en el DataFrame
         if mes not in data.columns:
-            raise KeyError("El nombre de la columna '{}' no es válido. Por favor, asegúrate de ingresar el nombre correcto de la columna. ".format(mes))
+            raise KeyError(f"El nombre de la columna '{mes}' no es válido. Por favor, asegúrate de ingresar el nombre correcto de la columna.")
 
         # Filtra el DataFrame por el mes especificado
         data_filtered = data[mes]
@@ -30,16 +29,12 @@ def Clientes_por_Mes(mes: str):
         if not data_filtered.empty:
             # Realiza una función de agregación para el mes especificado
             numero_clientes = data_filtered.sum()
-            return {
-                "El número de clientes para el mes {} es:".format(mes): numero_clientes
-            }
+            return {"El número de clientes para el mes {} es:".format(mes): numero_clientes}
         else:
-            return {
-                "El número de clientes para el mes {} es:".format(mes): 0  # Si no hay clientes, el número es cero
-            }
-    except KeyError:
+            return {"El número de clientes para el mes {} es:".format(mes): 0}  # Si no hay clientes, el número es cero
+    
+    except KeyError as e:
         # Si hay un error de clave (KeyError), proporciona mensajes de error
-        # Calculamos el mínimo y el máximo de las fechas si las columnas existen
         if 'Mes Registro' in data.columns and 'Mes de Abandono (Churn)' in data.columns:
             fecha_minima = data['Mes Registro'].min()
             fecha_maxima = data['Mes de Abandono (Churn)'].max()
@@ -48,100 +43,117 @@ def Clientes_por_Mes(mes: str):
             fecha_minima_truncada = fecha_minima.strftime('%Y-%m')
             fecha_maxima_truncada = fecha_maxima.strftime('%Y-%m')
 
-            
-            print("El nombre de la columna '{}' no es válido. Por favor, asegúrate de ingresar el nombre correcto de la columna.".format(mes))
-            print("\nEl formato de las fechas es: M/YYYY. Los registros comienzan en {} y terminan en {}.".format(fecha_minima_truncada, fecha_maxima_truncada))
-
-        else:
-            
-            print("La columna a la que intentas ingresar no es valida")
+            return {
+                "error": f"La columna '{mes}' no es válida. Por favor, asegúrate de ingresar el nombre correcto de la columna.",
+                "formato_fechas": f"El formato de las fechas es: M/YYYY. Los registros comienzan en {fecha_minima_truncada} y terminan en {fecha_maxima_truncada}."
+            }
         
+        else:
+            return {"error": f"La columna '{mes}' no es válida. Además, las columnas de fecha necesarias no están presentes en el DataFrame."}        
 
 def Mes_mayorClientes(data):
-    # Diccionario para almacenar el recuento de nuevos clientes por mes
-    nuevos_clientes_por_mes = {}
+    # Obtener el total de clientes nuevos por mes de registro
+    clientes_por_mes = data.groupby('Mes Registro').size()
 
-    # Iterar sobre las columnas del DataFrame para contar los nuevos clientes por mes
-    for i, columna in enumerate(data.columns[4:], start=1):
-        # Filtrar clientes nuevos para el mes actual
-        clientes_mes_actual = data[columna].dropna()
-        # Excluir clientes que ya estaban registrados en el mes anterior (si corresponde)
-        if i > 1:  # Excluir el primer mes
-            clientes_mes_anterior = data[data.columns[i - 1]].dropna()
-            clientes_mes_actual = clientes_mes_actual[~clientes_mes_actual.isin(clientes_mes_anterior)]
-        # Contar el número de nuevos clientes para el mes actual
-        count = len(clientes_mes_actual)
-        # Almacenar el recuento en el diccionario
-        nuevos_clientes_por_mes[columna] = count
-
-    # Encontrar el mes con el segundo mayor recuento de nuevos clientes
-    mes_mas_clientes = sorted(nuevos_clientes_por_mes, key=nuevos_clientes_por_mes.get, reverse=True)[1]
-    num_clientes = nuevos_clientes_por_mes[mes_mas_clientes]
+    # Encontrar el mes con la mayor cantidad de nuevos clientes
+    mes_mas_clientes = clientes_por_mes.idxmax()  # Obtiene el índice (mes) con el máximo valor
+    num_clientes = clientes_por_mes.max()  # Obtiene el valor máximo (número de clientes)
 
     return {
         "Mes con mayor número de nuevos clientes": mes_mas_clientes,
         "Número de nuevos clientes en el mes": num_clientes
     }
 
-
 def Mes_mayor_abandono(data):
-    # Diccionario para almacenar el recuento de abandono de clientes por mes
-    abandono_por_mes = {}
+    # Obtener el total de clientes que abandonaron por mes
+    clientes_abandono_por_mes = data.groupby('Mes de Abandono (Churn)').size()
 
-    # Iterar sobre las columnas del DataFrame para contar el abandono de clientes por mes
-    for i, columna in enumerate(data.columns[4:], start=1):  # Excluir el primer mes
-        # Filtrar clientes activos para el mes actual
-        clientes_mes_actual = data[data[columna].isnull()]
-        #clientes_mes_actual = data[columna].dropna()
-        # Filtrar clientes activos para el mes anterior
-        if i > 1:
-            clientes_mes_anterior = data[data[data.columns[i-1]].notna()]
-            # Excluir clientes que ya habían abandonado en el mes anterior
-            clientes_mes_actual = clientes_mes_actual[~clientes_mes_actual.index.isin(clientes_mes_anterior.index)]
-        # Contar el número de clientes abandonados para el mes actual
-        count = len(clientes_mes_actual)
-        # Almacenar el recuento en el diccionario
-        abandono_por_mes[columna] = count
-
-    # Encontrar el mes con el mayor recuento de abandono de clientes
-    mes_con_mayor_abandono = max(abandono_por_mes, key=abandono_por_mes.get)
-    num_clientes_abandonados = abandono_por_mes[mes_con_mayor_abandono]
+    # Encontrar el mes con la mayor cantidad de abandono de clientes
+    mes_mas_abandono = clientes_abandono_por_mes.idxmax()  # Obtiene el índice (mes) con el máximo valor
+    num_clientes_abandono = clientes_abandono_por_mes.max()  # Obtiene el valor máximo (número de clientes que abandonaron)
 
     return {
-        "Mes con mayor abandono de clientes": mes_con_mayor_abandono,
-        "Número de clientes abandonados en ese mes": num_clientes_abandonados
+        "Mes con mayor número de clientes que abandonaron": mes_mas_abandono,
+        "Número de clientes que abandonaron en el mes": num_clientes_abandono
     }
 
+def Mes_Mayor_Porcentaje_Retencion(data, mes:str):
+    # Cuenta el número de clientes por mes de inicio
+    clientes_por_mes_inicio = data.groupby('Mes Registro').size()
 
-def Porcentaje_Retencion_Abandono(data):
-    # Inicializar listas para almacenar los porcentajes de retención y abandono mes a mes
-    porcentajes_retencion = []
-    porcentajes_abandono = []
+    # Cuenta el número de clientes por mes de fin
+    clientes_por_mes_fin = data.groupby('Mes de Abandono (Churn)').size()
 
-    # Iterar sobre las columnas del DataFrame
-    for columna in data.columns[4:]:
-        # Filtrar clientes activos y churned para el mes actual
-        clientes_activos = data[data[columna] == 1] #Esta activo cuando es 1
-        clientes_inactivos = data[data[columna].isna()] #inactivo cuando es NaN
+    # Ordena los datos por fecha
+    clientes_por_mes_fin = clientes_por_mes_fin.sort_index()
 
-        # Contar el número de clientes activos y abandonados para el mes actual
-        count_activos = len(clientes_activos)
-        count_abandonados = len(clientes_inactivos)
+    # Obtener el total de clientes activos hasta el mes dado
+    nuevos_clientes_hasta_mes = clientes_por_mes_inicio.loc[mes]
 
-        # Calcular el porcentaje de retención y abandono para el mes actual
-        total_clientes_mes = count_activos + count_abandonados
-        porcentaje_retencion_mes = (count_activos / total_clientes_mes) * 100
-        porcentaje_abandono_mes = (count_abandonados / total_clientes_mes) * 100
+    # Obtener el total de abandonos hasta el mes dado
+    abandonos_hasta_mes = clientes_por_mes_fin.loc[:mes].sum()
 
-        # Almacenar los porcentajes mes a mes
-        porcentajes_retencion.append(porcentaje_retencion_mes)
-        porcentajes_abandono.append(porcentaje_abandono_mes)
-
-    # Calcular el promedio de los porcentajes de retención y abandono de todos los meses
-    promedio_retencion = sum(porcentajes_retencion) / len(porcentajes_retencion)
-    promedio_abandono = sum(porcentajes_abandono) / len(porcentajes_abandono)
+    # Calcular el porcentaje de retención para el mes dado
+    porcentaje_retencion_mes = (((nuevos_clientes_hasta_mes - abandonos_hasta_mes) / len(data)) - 1) * -100
 
     return {
-        "Porcentaje promedio de retención de clientes": promedio_retencion,
-        "Porcentaje promedio de abandono de clientes": promedio_abandono
+        "Mes": mes,
+        "Porcentaje de retención para el mes dado": porcentaje_retencion_mes
+
     }
+
+def Mes_Mayor_Porcentaje_Retencion_Total(data):
+    # Diccionario para almacenar los porcentajes de retención para cada mes
+    porcentajes_retencion = {}
+
+    # Iterar sobre todos los meses en los datos
+    for mes in data['Mes Registro'].unique():
+        # Calcular el porcentaje de retención para el mes dado
+        resultado_mes = Mes_Mayor_Porcentaje_Retencion(data, mes)
+        
+        # Almacenar el porcentaje de retención para este mes en el diccionario
+        porcentajes_retencion[mes] = resultado_mes["Porcentaje de retención para el mes dado"]
+
+    # Encontrar el mes con el mayor porcentaje de retención
+    mes_mayor_retencion = max(porcentajes_retencion, key=porcentajes_retencion.get)
+    mayor_porcentaje_retencion = porcentajes_retencion[mes_mayor_retencion]
+
+    return {
+        "Mes con mayor porcentaje de retención": mes_mayor_retencion,
+        "Mayor porcentaje de retención": mayor_porcentaje_retencion
+    }
+
+def Mes_Mayor_Porcentaje_Abandono(data, mes):
+    # Calcular el porcentaje de retención para el mes dado
+    resultado_retencion = Mes_Mayor_Porcentaje_Retencion(data, mes)
+    porcentaje_retencion_mes = resultado_retencion["Porcentaje de retención para el mes dado"]
+
+    # Calcular el porcentaje de abandono como el inverso del porcentaje de retención
+    #Aplicando las bases estadisticas de Q=1-P
+    porcentaje_abandono_mes = (1- (porcentaje_retencion_mes/100)) * 100
+
+    return {
+        "Mes": mes,
+        "Porcentaje de abandono para el mes dado": porcentaje_abandono_mes
+    }
+
+'''def Mes_Mayor_Porcentaje_Abandono_Total(data):
+    # Diccionario para almacenar los porcentajes de abandono para cada mes
+    porcentajes_abandono = {}
+
+    # Iterar sobre todos los meses en los datos
+    for mes in data['Mes de Abandono (Churn)'].unique():
+        # Calcular el porcentaje de abandono para el mes dado
+        resultado_mes = Mes_Mayor_Porcentaje_Abandono(data, mes)
+        
+        # Almacenar el porcentaje de abandono para este mes en el diccionario
+        porcentajes_abandono[mes] = resultado_mes["Porcentaje de abandono para el mes dado"]
+
+    # Encontrar el mes con el mayor porcentaje de abandono
+    mes_mayor_abandono = max(porcentajes_abandono, key=porcentajes_abandono.get)
+    mayor_porcentaje_abandono = porcentajes_abandono[mes_mayor_abandono]
+
+    return {
+        "Mes con mayor porcentaje de abandono": mes_mayor_abandono,
+        "Mayor porcentaje de abandono": mayor_porcentaje_abandono
+    }'''
--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 from fastapi import FastAPI
-from API_Retencion import Clientes_por_Mes, Mes_mayorClientes, Mes_mayor_abandono, Porcentaje_Retencion_Abandono
-import requests
+from API_Retencion import (Clientes_por_Mes, Mes_mayorClientes, Mes_mayor_abandono, 
+                           Porcentaje_Retencion_Abandono, Mes_Mayor_Porcentaje_Retencion,
+                           Mes_Mayor_Porcentaje_Abandono, Mes_Mayor_Porcentaje_Retencion_Total)
 
 app_retencion = FastAPI()
 
 @app_retencion.get("/")
 def read_root():
-    return {"message": "¡Bienvenido a la API de Recomendaciones!"}
+    return {"message": "¡Bienvenido a la API de Retención!"}
 
 @app_retencion.get("/clientes-mes/{mes}")
 def obtener_clientes_por_mes(mes: str):
     result = Clientes_por_Mes(mes)
     return {"result": result}
 
-@app_retencion.get("/obtener-clientes-mes/{mes}")
-def obtener_clientes_mes(mes: str):
-    url = f'https://api-retenciontech.onrender.com/clientes-mes/{mes}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return {"error": "No se pudo obtener los datos"}
-
-
-
-'''@app_retencion.get("/user-for-genre/{genero}")
-def read_user_for_genre(genero: str):
-    result = UserForGenre(genero)
+@app_retencion.get("/mes-mayor-clientes")
+def obtener_mes_mayor_clientes():
+    result = Mes_mayorClientes(data)
     return {"result": result}
 
-@app_retencion.get("/users-recommend/{año}")
-def read_users_recommend(año: int):
-    result = UsersRecommend(año)
+@app_retencion.get("/mes-mayor-abandono")
+def obtener_mes_mayor_abandono():
+    result = Mes_mayor_abandono(data)
     return {"result": result}
 
-@app_retencion.get("/users-worst-developer/{año}")
-def read_users_worst_developer(año: int):
-    result = UsersWorstDeveloper(año)
-    return {"result": result}'''
+@app_retencion.get("/porcentaje-retencion-mes/{mes}")
+def obtener_porcentaje_retencion_mes(mes: str):
+    result = Mes_Mayor_Porcentaje_Retencion(data, mes)
+    return {"result": result}
+
+@app_retencion.get("/porcentaje-retencion-total")
+def obtener_porcentaje_retencion_total():
+    result = Mes_Mayor_Porcentaje_Retencion_Total(data)
+    return {"result": result}
+
+@app_retencion.get("/porcentaje-abandono-mes/{mes}")
+def obtener_porcentaje_abandono_mes(mes: str):
+    result = Mes_Mayor_Porcentaje_Abandono(data, mes)
+    return {"result": result}
+
