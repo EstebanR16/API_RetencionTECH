7fbcf37f684d35e4a45ae3dbc14ae42109ba398e
EstebanR16 <est1696@hotmail.com>
2024-03-26 10:47:29 -0600

    Modificacion de archivo main.py para cosultas con requests

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from API_Retencion import Clientes_por_Mes, Mes_mayorClientes, Mes_mayor_abandono, Porcentaje_Retencion_Abandono
+import requests
 
 app_retencion = FastAPI()
 
@@ -11,6 +12,16 @@ def read_root():
 def obtener_clientes_por_mes(mes: str):
     result = Clientes_por_Mes(mes)
     return {"result": result}
+
+@app_retencion.get("/obtener-clientes-mes/{mes}")
+def obtener_clientes_mes(mes: str):
+    url = f'https://api-retenciontech.onrender.com/clientes-mes/{mes}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": "No se pudo obtener los datos"}
+
 
 
 '''@app_retencion.get("/user-for-genre/{genero}")
